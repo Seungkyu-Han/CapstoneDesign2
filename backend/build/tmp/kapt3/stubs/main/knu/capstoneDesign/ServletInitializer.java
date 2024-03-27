2069369283168b57d6a920e9f00201d536e85308
trust1204@gmail.com <trust1204@gmail.com>
2024-03-27 11:13:26 +0900

    change git inore

---
+++ backend/build/tmp/kapt3/stubs/main/knu/capstoneDesign/ServletInitializer.java
@@ -0,0 +1,16 @@
+package knu.capstoneDesign;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0004H\u0014\u00a8\u0006\u0006"}, d2 = {"Lknu/capstoneDesign/ServletInitializer;", "Lorg/springframework/boot/web/servlet/support/SpringBootServletInitializer;", "()V", "configure", "Lorg/springframework/boot/builder/SpringApplicationBuilder;", "application", "capstoneDesign"})
+public final class ServletInitializer extends org.springframework.boot.web.servlet.support.SpringBootServletInitializer {
+    
+    public ServletInitializer() {
+        super();
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.NotNull()
+    protected org.springframework.boot.builder.SpringApplicationBuilder configure(@org.jetbrains.annotations.NotNull()
+    org.springframework.boot.builder.SpringApplicationBuilder application) {
+        return null;
+    }
+}
