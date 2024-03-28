79c1fcdf72f7b79995da5b4420f032ff507e0c81
trust1204@gmail.com <trust1204@gmail.com>
2024-03-28 11:39:55 +0900

    [backend] refactor: change image&container name

--- backend/startDocker-main.sh
+++ backend/startDocker-main.sh
@@ -1,5 +1,5 @@
 # docker 컨테이너 중지 및 이미지 삭제
-sudo docker rm shared-routine-main
+sudo docker rm capstone-main
 sudo docker rmi prune
 
 #최신버전 pull 후에 빌드
@@ -7,5 +7,5 @@ git pull origin main
 ./gradlew clean build -x test
 
 # docker 실행
-sudo docker build -t shared-routine:main .
-sudo docker run --name shared-routine-main -d -p 8080:8080 --rm shared-routine:main
+sudo docker build -t capstone:main .
+sudo docker run --name capstone-main -d -p 8080:8080 --rm capstone:main
