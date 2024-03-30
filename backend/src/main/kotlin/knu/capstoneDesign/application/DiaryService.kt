70665d79f5656af7b01009bd7de9a05ebfeba2ed
trust1204@gmail.com <trust1204@gmail.com>
2024-03-30 11:40:26 +0900

    [backend] feat: getDiary Api 추가

--- backend/src/main/kotlin/knu/capstoneDesign/application/DiaryService.kt
+++ backend/src/main/kotlin/knu/capstoneDesign/application/DiaryService.kt
@@ -1,11 +1,12 @@
 package knu.capstoneDesign.application
 
-import knu.capstoneDesign.data.dto.diary.DiaryPostReq
+import knu.capstoneDesign.data.dto.diary.req.DiaryPostReq
+import knu.capstoneDesign.data.dto.diary.res.DiaryGetRes
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import java.time.LocalDate
 
 interface DiaryService {
     fun post(diaryPostReq: DiaryPostReq): ResponseEntity<HttpStatusCode>
-
-
+    fun get(userId: Int, date: LocalDate):ResponseEntity<DiaryGetRes>
 }
