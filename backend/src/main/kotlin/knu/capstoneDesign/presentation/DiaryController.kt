330a7678f01e1f4650e2f58575d4ab0408866b73
trust1204@gmail.com <trust1204@gmail.com>
2024-03-30 11:40:26 +0900

    [backend] feat: getDiary Api 추가

--- backend/src/main/kotlin/knu/capstoneDesign/presentation/DiaryController.kt
+++ backend/src/main/kotlin/knu/capstoneDesign/presentation/DiaryController.kt
@@ -5,14 +5,18 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
-import knu.capstoneDesign.data.dto.diary.DiaryPostReq
+import knu.capstoneDesign.data.dto.diary.req.DiaryPostReq
 import knu.capstoneDesign.application.DiaryService
+import knu.capstoneDesign.data.dto.diary.res.DiaryGetRes
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/api/diary")
@@ -24,7 +28,16 @@ class DiaryController(private val diaryService: DiaryService) {
     @ApiResponses(
         ApiResponse(responseCode = "200", description = "성공", content = arrayOf(Content()))
     )
-    fun post(@RequestBody diaryPostReq:DiaryPostReq):ResponseEntity<HttpStatusCode>{
+    fun post(@RequestBody diaryPostReq: DiaryPostReq):ResponseEntity<HttpStatusCode>{
         return diaryService.post(diaryPostReq)
+    }
+
+    @GetMapping
+    @Operation(summary = "일기 조회 API", description = "일단 구현해놨고, 안에 상담 추가 내용 필요할 거 같은거 말씀해주시면 나중에 추가할게요")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "성공", content = arrayOf(Content()))
+    )
+    fun get(@RequestParam userId: Int, @RequestParam date: LocalDate):ResponseEntity<DiaryGetRes>{
+        return diaryService.get(userId, date)
     }
 }
--- backend/src/main/kotlin/knu/capstoneDesign/repository/DiaryRepository.kt
+++ backend/src/main/kotlin/knu/capstoneDesign/repository/DiaryRepository.kt
@@ -1,10 +1,12 @@
 package knu.capstoneDesign.repository
 
 import knu.capstoneDesign.data.entity.Diary
+import knu.capstoneDesign.data.entity.User
 import knu.capstoneDesign.repository.custom.DiaryRepositoryCustom
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDate
 
 interface DiaryRepository: JpaRepository<Diary, Int>, DiaryRepositoryCustom {
-
     fun findTopByOrderByIdDesc():Diary
+    fun findByUserAndDate(user: User, date: LocalDate): Diary
 }
