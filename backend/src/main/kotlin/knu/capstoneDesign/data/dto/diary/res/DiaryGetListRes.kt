b32cc8f3d2dc04b371dc84d13a4fb0290724539b
trust1204@gmail.com <trust1204@gmail.com>
2024-04-11 20:50:01 +0900

    [backend] feat: diary get response element에 LocalDate 추가

--- backend/src/main/kotlin/knu/capstoneDesign/data/dto/diary/res/DiaryGetListRes.kt
+++ backend/src/main/kotlin/knu/capstoneDesign/data/dto/diary/res/DiaryGetListRes.kt
@@ -1,8 +1,11 @@
 package knu.capstoneDesign.data.dto.diary.res
 
 import com.querydsl.core.annotations.QueryProjection
+import java.time.LocalDate
 
 data class DiaryGetListRes @QueryProjection constructor(
+    val id: Int,
+    val date: LocalDate,
     val title: String,
     val content: String
 )
