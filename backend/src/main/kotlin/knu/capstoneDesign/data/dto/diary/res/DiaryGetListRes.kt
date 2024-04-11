22e4a33208d874ad558823032a579380cd1710d2
trust1204@gmail.com <trust1204@gmail.com>
2024-04-11 17:48:03 +0900

    [backend] refactor: diary title column 추가

--- backend/src/main/kotlin/knu/capstoneDesign/data/dto/diary/res/DiaryGetListRes.kt
+++ backend/src/main/kotlin/knu/capstoneDesign/data/dto/diary/res/DiaryGetListRes.kt
@@ -3,5 +3,6 @@ package knu.capstoneDesign.data.dto.diary.res
 import com.querydsl.core.annotations.QueryProjection
 
 data class DiaryGetListRes @QueryProjection constructor(
+    val title: String,
     val content: String
 )
