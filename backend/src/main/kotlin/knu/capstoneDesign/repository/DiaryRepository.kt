4880b65706d99ed54d1f1db2189cc9525a11c0e1
trust1204@gmail.com <trust1204@gmail.com>
2024-04-20 10:11:07 +0900

    [backend] refactor: Diary 조회 리턴 타입 변경

--- backend/src/main/kotlin/knu/capstoneDesign/repository/DiaryRepository.kt
+++ backend/src/main/kotlin/knu/capstoneDesign/repository/DiaryRepository.kt
@@ -5,8 +5,9 @@ import knu.capstoneDesign.data.entity.User
 import knu.capstoneDesign.repository.custom.DiaryRepositoryCustom
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDate
+import java.util.*
 
 interface DiaryRepository: JpaRepository<Diary, Int>, DiaryRepositoryCustom {
-    fun findByUserAndDate(user: User, date: LocalDate): Diary
+    fun findByUserAndDate(user: User, date: LocalDate): Optional<Diary>
     fun deleteByUserAndDate(user: User, date: LocalDate): Int
 }
