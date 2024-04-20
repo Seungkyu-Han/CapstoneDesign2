005adc5ae1238e491b62139d3b3aa265523f489f
trust1204@gmail.com <trust1204@gmail.com>
2024-04-20 09:52:19 +0900

    [backend] refactor: get Api Parameter 변경

--- backend/src/main/kotlin/knu/capstoneDesign/application/DiaryService.kt
+++ backend/src/main/kotlin/knu/capstoneDesign/application/DiaryService.kt
@@ -9,7 +9,7 @@ import java.time.LocalDate
 
 interface DiaryService {
     fun post(diaryPostReq: DiaryPostReq): ResponseEntity<HttpStatusCode>
-    fun get(userId: Int, date: LocalDate):ResponseEntity<DiaryGetRes>
+    fun get(id: Int):ResponseEntity<DiaryGetRes>
     fun patch(diaryPostReq: DiaryPostReq): ResponseEntity<HttpStatusCode>
     fun delete(userId: Int, date: LocalDate): ResponseEntity<HttpStatusCode>
     fun getList(userId: Int, startDate: LocalDate, endDate: LocalDate): ResponseEntity<List<DiaryGetListRes>>
