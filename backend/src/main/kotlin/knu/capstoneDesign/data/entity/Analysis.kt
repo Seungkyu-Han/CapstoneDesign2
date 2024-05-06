a205504d55ee56ef33b864119a1dd93931dedaf4
trust1204@gmail.com <trust1204@gmail.com>
2024-05-06 11:37:22 +0900

    [backend] refactor: Analysis Entity 수정

--- backend/src/main/kotlin/knu/capstoneDesign/data/entity/Analysis.kt
+++ backend/src/main/kotlin/knu/capstoneDesign/data/entity/Analysis.kt
@@ -1,12 +1,13 @@
 package knu.capstoneDesign.data.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.OneToOne
+import jakarta.persistence.*
 
 @Entity
 data class Analysis(
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private val id: Long?,
+
     @OneToOne
     private var diary:Diary,
     private var isPositive: Boolean,
