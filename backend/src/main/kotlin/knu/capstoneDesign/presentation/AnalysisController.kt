1b63166dacfae01341f51512d8f88abd75ceea68
trust1204@gmail.com <trust1204@gmail.com>
2024-05-06 11:38:41 +0900

    [backend] feat: 감정분석 API 생성

--- backend/src/main/kotlin/knu/capstoneDesign/presentation/AnalysisController.kt
+++ backend/src/main/kotlin/knu/capstoneDesign/presentation/AnalysisController.kt
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import knu.capstoneDesign.application.AnalysisService
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
@@ -17,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/feeling")
 @Tag(name = "감정")
-class AnalysisController(private val analysisService: AnalysisService) {
+class AnalysisController(
+    @Qualifier("analysisServiceAuthImpl") private val analysisService: AnalysisService) {
 
     @GetMapping
     @Operation(summary = "감정 분석 API")
