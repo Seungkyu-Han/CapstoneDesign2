023814fb6d2a7baafae9856d57b6e167f13feeb9
thanhhien234 <doanthanhhien01@gmail.com>
2024-05-10 00:19:51 +0900

    [frontend] fix: change css

--- frontend/stt_diary/src/routes/SentimentResult.js
+++ frontend/stt_diary/src/routes/SentimentResult.js
@@ -13,12 +13,12 @@ function SentimentResult() {
   return (
     <div className='sentiment-result-container'>
         <div className={`sentiment-result-inner ${resultClassName}`}>
-            <h3 className='result-title-container'>
-                <span className='title1'>감정 분석 결과는 </span>
+            <h3 className='result-title'>
+                감정 분석 결과는
                 {result === 1 && <span className='title-positive'>긍정적</span>}
                 {result === 2 && <span className='title-negative'>부정적</span>}
                 {result === 3 && <span className='title-neutral'>중립적</span>}
-                <span className='title1'>이네요 </span>
+                이네요
             </h3>
             <div className='result-content'>
                 <p>... 감정분석 내용 ...... 감정분석 내용 ...... 감정분석 내용 ...... 감정분석 내용 ...... 감정분석 내용 ...... 감정분석 내용 ...... 감정분석 내용 ...감정분석 내용 ...... </p>
