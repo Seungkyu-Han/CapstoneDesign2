9d99c4c5685da468e04cdf947358f8f38c322051
thanhhien234 <doanthanhhien01@gmail.com>
2024-05-15 00:54:37 +0900

    [frondend] fix: change div -> a

--- frontend/stt_diary/src/routes/Login.js
+++ frontend/stt_diary/src/routes/Login.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './Login.css';
 
 function Login() {
-    const login = () => {
-        window.location.href = `https://kauth.kakao.com/oauth/authorize?&response_type=code&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}/redirect`;
-    };
+    const url = `https://kauth.kakao.com/oauth/authorize?&response_type=code&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}/redirect`;
     return (
         <div className='login-container'>
             <img className='img1' src={require('../assets/login1.png')} alt='' />
             <img className='img2' src={require('../assets/login2.png')} alt='' />
-            <div className='login-btn' onClick={login}>
-            <img src={require('../assets/kakaoBtn.png')} alt='' />
-            </div>
+            <a href={url} className='login-btn'>
+                <img src={require('../assets/kakaoBtn.png')} alt=''/>
+            </a>
         </div>
     );
 }
