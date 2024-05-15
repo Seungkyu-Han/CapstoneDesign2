1cd659aeeb75f3970149d32c8ffdb4fc46a7a4f2
Seungkyu-Han <trust1204@gmail.com>
2024-05-15 15:05:03 +0900

    [backend] refactor: change Consulting Entity column

--- backend/src/main/kotlin/knu/capstoneDesign/data/entity/Consulting.kt
+++ backend/src/main/kotlin/knu/capstoneDesign/data/entity/Consulting.kt
@@ -1,17 +1,17 @@
 package knu.capstoneDesign.data.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.OneToOne
+import jakarta.persistence.*
 
 @Entity
 data class Consulting(
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int?,
+
+
     @OneToOne
-    private var diary: Diary,
+    var diary: Diary,
 
     @Column(length = 1000)
-    private var content:String
+    var content:String
 )
