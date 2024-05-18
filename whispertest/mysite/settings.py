1c0cd7104d5b910c47b9541eb559afc3ac8076fa
2sangguhyun <hsh002819@gmail.com>
2024-05-18 17:37:48 +0900

    cors header

--- whispertest/mysite/settings.py
+++ whispertest/mysite/settings.py
@@ -58,9 +58,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'fileupload',
     'bootstrap4',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,6 +72,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'mysite.urls'
 
