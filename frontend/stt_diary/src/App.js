65a42f0b2f2eb062cbd7a87f2784c0350cdfb937
Jonghyeok <jhpmuscle@gmail.com>
2024-05-25 21:38:42 +0900

    fix: cancle login check comment

--- frontend/stt_diary/src/App.js
+++ frontend/stt_diary/src/App.js
@@ -23,31 +23,31 @@ function App() {
   };
 
   useEffect(() => {
-    // if (!getCookie("accessToken") && !getCookie("refreshToken")) {
-    //   navigate('/login');
-    // }
-    // else if (!getCookie("accessToken")) {
-    //     fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
-    //       method: 'PATCH',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //         'Authorization': `Bearer ${getCookie("refreshToken")}`
-    //       }
-    //       })         
-    //       .then(response => response.json())
-    //       .then(res => {
-    //         setCookie("accessToken", res.accessToken, 2 * 60);
-    //         setCookie("refreshToken", res.refreshToken, 24 * 14 * 60);
-    //         setLoggedIn(true);
-    //     })
-    //     .catch(error => {
-    //         alert("관리자에게 문의해주세요.");
-    //     });
-    // }
-    // else {
-    //   setLoggedIn(true);
-    //   navigate('/');
-    // }
+    if (!getCookie("accessToken") && !getCookie("refreshToken")) {
+      navigate('/login');
+    }
+    else if (!getCookie("accessToken")) {
+        fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${getCookie("refreshToken")}`
+          }
+          })         
+          .then(response => response.json())
+          .then(res => {
+            setCookie("accessToken", res.accessToken, 2 * 60);
+            setCookie("refreshToken", res.refreshToken, 24 * 14 * 60);
+            setLoggedIn(true);
+        })
+        .catch(error => {
+            alert("관리자에게 문의해주세요.");
+        });
+    }
+    else {
+      setLoggedIn(true);
+      navigate('/');
+    }
   },[loggedIn]);
 
 
