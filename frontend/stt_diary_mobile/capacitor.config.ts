6df1f11f6bca08e73c72243bcc92749efd8389c4
thanhhien234 <doanthanhhien01@gmail.com>
2024-06-25 18:29:55 +0900

    [frontend] fix: remove unnecessary files

--- frontend/stt_diary_mobile/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'SoundDiary',
-  webDir: 'build'
-};
-
-export default config;
